8708bd5ddbe77486257385da99315f181660620d
Андрей Лыткин <andrey66-66-66@yandex.ru>
2023-04-30 17:07:36 +0300

    Create new attributes

---
+++ src/main/java/com/andrey66/dimasrpg/attribute/custom/MagicArmorAttribute.java
@@ -0,0 +1,11 @@
+package com.andrey66.dimasrpg.attribute.custom;
+
+import net.minecraft.world.entity.ai.attributes.Attribute;
+
+// Класс для нового аттрибута магической защиты
+public class MagicArmorAttribute extends Attribute {
+
+        public MagicArmorAttribute(String p_22080_, double p_22081_) {
+            super(p_22080_, p_22081_);
+        }
+}
--- src/main/java/com/andrey66/dimasrpg/attribute/custom/MeleeArmorAttribute.java
+++ src/main/java/com/andrey66/dimasrpg/attribute/custom/MeleeArmorAttribute.java
@@ -3,9 +3,9 @@ package com.andrey66.dimasrpg.attribute.custom;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 
 // Класс для нового аттрибута магической защиты
-public class MagicResAttribute extends Attribute {
+public class MeleeArmorAttribute extends Attribute {
 
-        public MagicResAttribute(String p_22080_, double p_22081_) {
-            super(p_22080_, p_22081_);
-        }
+    public MeleeArmorAttribute(String p_22080_, double p_22081_) {
+        super(p_22080_, p_22081_);
+    }
 }
