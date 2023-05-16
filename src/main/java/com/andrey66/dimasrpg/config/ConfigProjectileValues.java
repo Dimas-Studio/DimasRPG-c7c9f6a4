961a87e6ce880668500676963d9b3b16b53428a4
Андрей Лыткин <andrey66-66-66@yandex.ru>
2023-05-16 22:33:50 +0300

    Create ConfigProjectileValues

---
+++ src/main/java/com/andrey66/dimasrpg/config/ConfigProjectileValues.java
@@ -0,0 +1,62 @@
+package com.andrey66.dimasrpg.config;
+
+import java.util.*;
+
+// Класс определения структуры конфига снарядов
+public class ConfigProjectileValues {
+
+    // Хрант в себе словарь из конфиг файла
+    private static final HashMap<String, HashMap<String, Float>> CONFIG_SPEC = new HashMap<>();
+
+    // Метод настройки конфига по умолчанию
+    public static void setDefaultConfigValues() {
+        put("minecraft:arrow", "range", 1);
+        put("minecraft:fireball", "range", 400);
+    }
+
+    // Метод очистки настроек конфига (используется для очистки настроек по умолчанию для замены их настройками их файла)
+    public static void clearDefaultConfigValues() {
+        CONFIG_SPEC.clear();
+    }
+
+    // метод добавления нового поля конифга
+    public static void put(String name, String type, float value) {
+        HashMap<String, Float> innerMap = new HashMap<>();
+        innerMap.put(type, value);
+        CONFIG_SPEC.put(name, innerMap);
+    }
+
+    // Метод получения типа урона предмета
+    public static String getType(String name) {
+        if(CONFIG_SPEC.containsKey(name)){
+            String type;
+            Set<String> keys = CONFIG_SPEC.get(name).keySet();
+            type = keys.iterator().next();
+            return type;
+        }
+        return null;
+    }
+
+
+    // метод пролучения количества урона предмета
+    public static Float getValue(String name) {
+        if(CONFIG_SPEC.containsKey(name)){
+            String type;
+            Set<String> keys = CONFIG_SPEC.get(name).keySet();
+            type = keys.iterator().next();
+            return CONFIG_SPEC.get(name).get(type);
+        }
+        return null;
+    }
+
+    // Метод получения множества ключей (имён предметов) из конфига
+    public static Set<String> getKeys() {
+        return CONFIG_SPEC.keySet();
+    }
+
+    // Метод проверки ключа (имени) на существование
+    public static Boolean exist(String name) {
+        return CONFIG_SPEC.containsKey(name);
+    }
+
+}
