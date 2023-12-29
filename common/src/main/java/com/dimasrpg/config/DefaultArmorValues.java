aac72e63f1ee6c0c18dfa0a41aeb71a01c25f87e
PiiosS <93620378+PiiosS@users.noreply.github.com>
2023-12-29 22:44:55 +0400

    Add multi-type default values for armor

--- common/src/main/java/com/dimasrpg/config/DefaultArmorValues.java
+++ common/src/main/java/com/dimasrpg/config/DefaultArmorValues.java
@@ -6,6 +6,8 @@ public class DefaultArmorValues {
         ArmorConfigValues.put("minecraft:chainmail_chestplate", "melee", 20);
         ArmorConfigValues.put("minecraft:chainmail_leggings", "melee", 40);
         ArmorConfigValues.put("minecraft:chainmail_helmet", "range", 30);
+        ArmorConfigValues.put("minecraft:chainmail_helmet", "admin", 30);
+        ArmorConfigValues.put("minecraft:chainmail_helmet", "magic", 30);
         ArmorConfigValues.put("minecraft:chainmail_boots", "melee", 5);
     }
 }
