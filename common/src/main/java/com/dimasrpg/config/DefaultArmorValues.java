a4ad41f8e87c25d3908288bbbd4df9f101b6cbf4
PiiosS <93620378+PiiosS@users.noreply.github.com>
2024-01-05 15:02:03 +0400

    Add a description of the file

--- common/src/main/java/com/dimasrpg/config/DefaultArmorValues.java
+++ common/src/main/java/com/dimasrpg/config/DefaultArmorValues.java
@@ -3,12 +3,18 @@ package com.dimasrpg.config;
 
 public class DefaultArmorValues {
     public static void init(){
-        ArmorConfigValues.put("minecraft:chainmail_chestplate", "melee", 20);
-        ArmorConfigValues.put("minecraft:chainmail_leggings", "melee", 40);
-        ArmorConfigValues.put("minecraft:chainmail_helmet", "range", 30);
-        ArmorConfigValues.put("minecraft:chainmail_helmet", "admin", 30);
-        ArmorConfigValues.put("minecraft:chainmail_helmet", "magic", 30);
-        ArmorConfigValues.put("minecraft:chainmail_boots", "melee", 5);
+        ArmorConfigValues.put("minecraft:chainmail_chestplate", "melee", 20.0F);
+        ArmorConfigValues.put("minecraft:chainmail_leggings", "melee", 40.0F);
+        ArmorConfigValues.put("minecraft:chainmail_helmet", "range", 30.0F);
+        ArmorConfigValues.put("minecraft:chainmail_helmet", "admin", 30.0F);
+        ArmorConfigValues.put("minecraft:chainmail_helmet", "magic", 30.0F);
+        ArmorConfigValues.put("minecraft:chainmail_boots", "melee", 5.0F);
     }
 }
 
+    /*
+    *   Здесь описываются стандартные значения для
+    *   Если нет файла конфигурации, то будут использоваться эти значения
+    *
+    *
+     */
