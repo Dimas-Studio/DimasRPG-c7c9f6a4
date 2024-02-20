d2553ae738bd8b1030fa933beaca4c67d58a7593
Andrey-66 <andrey66-66-66@yandex.ru>
2024-02-20 17:38:30 +0300

    Fix 6

--- common/src/main/java/com/dimasrpg/config/ConfigValuesInterface.java
+++ common/src/main/java/com/dimasrpg/config/ConfigValuesInterface.java
@@ -2,8 +2,14 @@ package com.dimasrpg.config;
 
 
 public interface ConfigValuesInterface {
-    public static void put(String name, String type, float value) {}
-    public static void clearDefaultConfigValues() {}
-    public static float getValue(String name, String type) {return 0.0F;}
-    public static String[] getTypes() {return new String[0];}
+    /**
+     * Добавляет значения в словарь.
+     * @param name Название предмета/брони
+     * @param type Тип урона/брони
+     * @param value Количество урона/брони
+     */
+    private static void put(String name, String type, float value) { }
+    private static void clearDefaultConfigValues() { }
+    private static float getValue(String name, String type) { return 0.0F; }
+    private static String[] getTypes() { return new String[0]; }
 }
